759574222b694c9861bc35d18500f3f8313af663
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-01 23:58:36 +0600

    not calc

--- app.js
+++ app.js
@@ -4,40 +4,10 @@ const for10percent = 300000;
 const for15percent = 400000;
 const for20percent = 500000;
 
-const calculateTax = income => {
-    let tax = 0;
-    // >1600000
-    if (income > (minimumIncome + for05percent + for10percent + for15percent + for20percent)) {
-        tax += (income - (minimumIncome + for05percent + for10percent + for15percent + for20percent)) * 0.25;
-        income = (minimumIncome + for05percent + for10percent + for15percent + for20percent);
-    }
-    // >1100000
-    if (income > (minimumIncome + for05percent + for10percent + for15percent)) {
-        tax += (income - (minimumIncome + for05percent + for10percent + for15percent)) * 0.2;
-        income = (minimumIncome + for05percent + for10percent + for15percent);
-    }
-    // >700000
-    if (income > (minimumIncome + for05percent + for10percent)) {
-        tax += (income - (minimumIncome + for05percent + for10percent)) * 0.15;
-        income = (minimumIncome + for05percent + for10percent);
-    }
-    // >400000
-    if (income > (minimumIncome + for05percent)) {
-        tax += (income - (minimumIncome + for05percent)) * 0.10;
-        income = (minimumIncome + for05percent);
-    }
-    // >300000
-    if (income > (minimumIncome)) {
-        tax += (income - (minimumIncome)) * 0.05;
-    }
-    return tax;
-};
-
 document.getElementById('button-input').addEventListener('click', () => {
     showTaxResult('');
     const input = document.getElementById('income-input');
     const taxType = document.getElementById('inputGroupSelect');
-    console.log(typeof taxType.value);
 
     if (taxType.value === '0') {
         showTaxResult('Please choose your tax type and input your tax payable income in number');
@@ -67,8 +37,36 @@ document.getElementById('button-input').addEventListener('click', () => {
 
 });
 
+const calculateTax = income => {
+    let tax = 0;
+    // >1600000
+    if (income > (minimumIncome + for05percent + for10percent + for15percent + for20percent)) {
+        tax += (income - (minimumIncome + for05percent + for10percent + for15percent + for20percent)) * 0.25;
+        income = (minimumIncome + for05percent + for10percent + for15percent + for20percent);
+    }
+    // >1100000
+    if (income > (minimumIncome + for05percent + for10percent + for15percent)) {
+        tax += (income - (minimumIncome + for05percent + for10percent + for15percent)) * 0.2;
+        income = (minimumIncome + for05percent + for10percent + for15percent);
+    }
+    // >700000
+    if (income > (minimumIncome + for05percent + for10percent)) {
+        tax += (income - (minimumIncome + for05percent + for10percent)) * 0.15;
+        income = (minimumIncome + for05percent + for10percent);
+    }
+    // >400000
+    if (income > (minimumIncome + for05percent)) {
+        tax += (income - (minimumIncome + for05percent)) * 0.10;
+        income = (minimumIncome + for05percent);
+    }
+    // >300000
+    if (income > (minimumIncome)) {
+        tax += (income - (minimumIncome)) * 0.05;
+    }
+    return tax;
+};
+
 const showTaxResult = messages => {
-    console.log(messages);
     document.getElementById('tax-result').innerText = messages;
 }
 
